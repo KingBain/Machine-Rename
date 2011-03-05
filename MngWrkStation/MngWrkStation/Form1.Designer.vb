898a4c9b7baade49c30e55d8c362c6ccc5e2f65c
John Bain <john.bain@gmail.com>
2011-03-04 22:45:52 -0600

    Added new buttons, set basic commands for radio buttons, and machine rename function is currently working
    Signed-off-by: John Bain <john.bain@gmail.com>

--- MngWrkStation/MngWrkStation/Form1.Designer.vb
+++ MngWrkStation/MngWrkStation/Form1.Designer.vb
@@ -32,13 +32,17 @@ Partial Class Form1
         Me.txtCurCompName = New System.Windows.Forms.TextBox()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnOK = New System.Windows.Forms.Button()
+        Me.btnCan = New System.Windows.Forms.Button()
+        Me.cmbContainer = New System.Windows.Forms.ComboBox()
+        Me.lblStatus = New System.Windows.Forms.Label()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'rbutWrkGrp
         '
         Me.rbutWrkGrp.AutoSize = True
-        Me.rbutWrkGrp.Location = New System.Drawing.Point(383, 45)
+        Me.rbutWrkGrp.Location = New System.Drawing.Point(116, 101)
         Me.rbutWrkGrp.Name = "rbutWrkGrp"
         Me.rbutWrkGrp.Size = New System.Drawing.Size(165, 17)
         Me.rbutWrkGrp.TabIndex = 8
@@ -49,7 +53,7 @@ Partial Class Form1
         'rbutDomain
         '
         Me.rbutDomain.AutoSize = True
-        Me.rbutDomain.Location = New System.Drawing.Point(383, 29)
+        Me.rbutDomain.Location = New System.Drawing.Point(116, 85)
         Me.rbutDomain.Name = "rbutDomain"
         Me.rbutDomain.Size = New System.Drawing.Size(172, 17)
         Me.rbutDomain.TabIndex = 9
@@ -60,7 +64,7 @@ Partial Class Form1
         'rbutRename
         '
         Me.rbutRename.AutoSize = True
-        Me.rbutRename.Location = New System.Drawing.Point(383, 12)
+        Me.rbutRename.Location = New System.Drawing.Point(116, 68)
         Me.rbutRename.Name = "rbutRename"
         Me.rbutRename.Size = New System.Drawing.Size(204, 17)
         Me.rbutRename.TabIndex = 10
@@ -71,7 +75,7 @@ Partial Class Form1
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(61, 40)
+        Me.Label2.Location = New System.Drawing.Point(6, 37)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(100, 13)
         Me.Label2.TabIndex = 7
@@ -80,7 +84,7 @@ Partial Class Form1
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(75, 14)
+        Me.Label1.Location = New System.Drawing.Point(20, 11)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(86, 13)
         Me.Label1.TabIndex = 6
@@ -88,14 +92,16 @@ Partial Class Form1
         '
         'txtCurDomain
         '
-        Me.txtCurDomain.Location = New System.Drawing.Point(171, 40)
+        Me.txtCurDomain.Enabled = False
+        Me.txtCurDomain.Location = New System.Drawing.Point(116, 37)
         Me.txtCurDomain.Name = "txtCurDomain"
         Me.txtCurDomain.Size = New System.Drawing.Size(197, 20)
         Me.txtCurDomain.TabIndex = 4
         '
         'txtCurCompName
         '
-        Me.txtCurCompName.Location = New System.Drawing.Point(171, 14)
+        Me.txtCurCompName.Enabled = False
+        Me.txtCurCompName.Location = New System.Drawing.Point(116, 11)
         Me.txtCurCompName.Name = "txtCurCompName"
         Me.txtCurCompName.Size = New System.Drawing.Size(197, 20)
         Me.txtCurCompName.TabIndex = 5
@@ -104,9 +110,9 @@ Partial Class Form1
         '
         Me.PictureBox1.BackColor = System.Drawing.Color.Black
         Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
-        Me.PictureBox1.Location = New System.Drawing.Point(0, 88)
+        Me.PictureBox1.Location = New System.Drawing.Point(0, 178)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(656, 79)
+        Me.PictureBox1.Size = New System.Drawing.Size(330, 79)
         Me.PictureBox1.TabIndex = 11
         Me.PictureBox1.TabStop = False
         '
@@ -116,17 +122,60 @@ Partial Class Form1
         Me.Label3.BackColor = System.Drawing.Color.Black
         Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label3.ForeColor = System.Drawing.Color.White
-        Me.Label3.Location = New System.Drawing.Point(115, 99)
+        Me.Label3.Location = New System.Drawing.Point(99, 178)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(224, 29)
         Me.Label3.TabIndex = 12
         Me.Label3.Text = "Machine Renamer"
         '
+        'btnOK
+        '
+        Me.btnOK.Enabled = False
+        Me.btnOK.Location = New System.Drawing.Point(23, 62)
+        Me.btnOK.Name = "btnOK"
+        Me.btnOK.Size = New System.Drawing.Size(75, 23)
+        Me.btnOK.TabIndex = 13
+        Me.btnOK.Text = "Ok"
+        Me.btnOK.UseVisualStyleBackColor = True
+        '
+        'btnCan
+        '
+        Me.btnCan.Location = New System.Drawing.Point(23, 95)
+        Me.btnCan.Name = "btnCan"
+        Me.btnCan.Size = New System.Drawing.Size(75, 23)
+        Me.btnCan.TabIndex = 13
+        Me.btnCan.Text = "Close"
+        Me.btnCan.UseVisualStyleBackColor = True
+        '
+        'cmbContainer
+        '
+        Me.cmbContainer.Enabled = False
+        Me.cmbContainer.FormattingEnabled = True
+        Me.cmbContainer.Location = New System.Drawing.Point(23, 133)
+        Me.cmbContainer.Name = "cmbContainer"
+        Me.cmbContainer.Size = New System.Drawing.Size(290, 21)
+        Me.cmbContainer.TabIndex = 14
+        '
+        'lblStatus
+        '
+        Me.lblStatus.AutoSize = True
+        Me.lblStatus.BackColor = System.Drawing.Color.Black
+        Me.lblStatus.ForeColor = System.Drawing.Color.White
+        Me.lblStatus.Location = New System.Drawing.Point(104, 211)
+        Me.lblStatus.Name = "lblStatus"
+        Me.lblStatus.Size = New System.Drawing.Size(81, 13)
+        Me.lblStatus.TabIndex = 15
+        Me.lblStatus.Text = "Program loaded"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(656, 169)
+        Me.ClientSize = New System.Drawing.Size(327, 257)
+        Me.Controls.Add(Me.lblStatus)
+        Me.Controls.Add(Me.cmbContainer)
+        Me.Controls.Add(Me.btnCan)
+        Me.Controls.Add(Me.btnOK)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.rbutWrkGrp)
@@ -137,7 +186,8 @@ Partial Class Form1
         Me.Controls.Add(Me.txtCurDomain)
         Me.Controls.Add(Me.txtCurCompName)
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Machine Renamer"
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
@@ -152,5 +202,9 @@ Partial Class Form1
     Friend WithEvents txtCurCompName As System.Windows.Forms.TextBox
     Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
     Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents btnOK As System.Windows.Forms.Button
+    Friend WithEvents btnCan As System.Windows.Forms.Button
+    Friend WithEvents cmbContainer As System.Windows.Forms.ComboBox
+    Friend WithEvents lblStatus As System.Windows.Forms.Label
 
 End Class
--- MngWrkStation/MngWrkStation/Form1.vb
+++ MngWrkStation/MngWrkStation/Form1.vb
@@ -1,6 +1,6 @@
 ﻿
 Public Class Form1
-    Dim objNetwork, strComputer, objComputer
+    Dim objNetwork, strComputer, objComputer, ErrCode
     Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         objNetwork = CreateObject("WScript.Network")
         strComputer = objNetwork.ComputerName
@@ -11,7 +11,37 @@ Public Class Form1
             Label2.Text = "Curent Domain:"
         End If
 
-
-        txtCurCompName.Text = strComputer
+        txtCurCompName.Text = objNetwork.ComputerName
+        txtCurDomain.Text = objComputer.Domain
     End Sub
+
+    Private Sub btnCan_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCan.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnOK_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnOK.Click
+
+        ErrCode = objComputer.Rename(txtCurCompName.Text)
+        If ErrCode = 0 Then
+            lblStatus.Text = "Computer renamed correctly."
+        Else
+            lblStatus.Text = "Error changing computer name."
+        End If
+
+    End Sub
+
+
+
+
+    Private Sub rbutRename_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles rbutRename.CheckedChanged
+        txtCurCompName.Enabled = True
+        txtCurCompName.Focus()
+        btnOK.Enabled = True
+
+
+
+
+    End Sub
+
+
 End Class
